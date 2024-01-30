65e387e77de794b5fcfe0e94880023a0763675e2
jakobniinja <dansaren124@outlook.com>
2024-01-30 17:35:16 +0100

    Fours WIP, no debugging needed lesson

--- src/main/java/com/jakobniinja/Yatzy.java
+++ src/main/java/com/jakobniinja/Yatzy.java
@@ -2,72 +2,128 @@ package com.jakobniinja;
 
 public class Yatzy {
 
-    public Yatzy() {
-        //
+  public Yatzy() {
+    //
+  }
+
+  int[] dices;
+
+  public Yatzy(int d0, int d1, int d2, int d3, int d4) {
+    dices = new int[5];
+
+    dices[0] = d0;
+    dices[1] = d1;
+    dices[2] = d2;
+    dices[3] = d3;
+    dices[4] = d4;
+  }
+
+  public static int yatzy(int... dice) {
+    int[] counts = new int[6];
+
+    for (int die : dice) {
+      counts[die - 1]++;
     }
 
-    int[] dices;
-
-    public Yatzy(int d0, int d1, int d2, int d3, int d4) {
-        dices = new int[5];
-
-        dices[0] = d0;
-        dices[1] = d1;
-        dices[2] = d2;
-        dices[3] = d3;
-        dices[4] = d4;
+    for (int i = 0; i != 6; i++) {
+      if (counts[i] == 5) {
+        return 50;
+      }
     }
 
-    public static int yatzy(int... dice) {
-        int[] counts = new int[6];
+    return 0;
+  }
 
-        for (int die : dice) {
-            counts[die - 1]++;
-        }
+  public int getDices() {
+    return dices.length;
+  }
 
-        for (int i = 0; i != 6; i++) {
-            if (counts[i] == 5) {
-                return 50;
-            }
-        }
+  public int chance(int d0, int d1, int d2, int d3, int d4) {
+    int total = 0;
+    total += d0;
+    total += d1;
+    total += d2;
+    total += d3;
+    total += d4;
 
-        return 0;
+    return total;
+  }
+
+
+  public static int ones(int d, int d1, int d2, int d3, int d4) {
+    int total = 0;
+    if (d == 1) {
+      total++;
+    }
+    if (d1 == 1) {
+      total++;
+    }
+    if (d2 == 1) {
+      total++;
+    }
+    if (d3 == 1) {
+      total++;
+    }
+    if (d4 == 1) {
+      total++;
     }
 
-    public int getDices() {
-        return dices.length;
+    return total;
+  }
+
+  public int twos(int d, int d1, int d2, int d3, int d4) {
+
+    int total = 0;
+
+    if (d == 2) {
+      total += 2;
+    }
+    if (d1 == 2) {
+      total += 2;
+    }
+    if (d2 == 2) {
+      total += 2;
+    }
+    if (d3 == 2) {
+      total += 2;
     }
 
-    public int chance(int d0, int d1, int d2, int d3, int d4) {
-        int total = 0;
-        total += d0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-
-        return total;
+    if (d4 == 2) {
+      total += 2;
     }
 
+    return total;
+  }
 
-    public static int ones(int d, int d1, int d2, int d3, int d4) {
-        int total = 0;
-        if (d == 1) {
-            total++;
-        }
-        if (d1 == 1) {
-            total++;
-        }
-        if (d2 == 1) {
-            total++;
-        }
-        if (d3 == 1) {
-            total++;
-        }
-        if (d4 == 1) {
-            total++;
-        }
-
-        return total;
+  public int threes(int d, int d1, int d2, int d3, int d4) {
+    int total =0;
+    if (d == 3) {
+      total += 3;
     }
+    if (d1 == 3) {
+      total += 3;
+    }
+    if (d2 == 3) {
+      total += 3;
+    }
+    if (d3 == 3) {
+      total += 3;
+    }
+    if (d4 == 3) {
+      total+=3;
+    }
+    return total;
+  }
+
+  public int fours() {
+
+    int sum =0;
+    for (int i = 0; i !=5; i++) {
+      if (dices[i] == 4) {
+        sum += 4;
+      }
+    }
+
+    return sum;
+  }
 }
--- src/test/java/com/jakobniinja/YatzyTest.java
+++ src/test/java/com/jakobniinja/YatzyTest.java
@@ -8,124 +8,186 @@ import org.junit.Test;
 
 public class YatzyTest {
 
-    private Yatzy yatzy;
+  private Yatzy yatzy;
 
-    @Before
-    public void setUp() {
-        yatzy = new Yatzy(1, 2, 3, 4, 5);
-    }
+  @Before
+  public void setUp() {
+    yatzy = new Yatzy(1, 2, 3, 4, 5);
+  }
 
-    @Test
-    public void onNoArgs() {
-        assertNotNull(new Yatzy());
-    }
-
-
-    @Test
-    public void on5ArgsConstructor() {
-        assertNotNull(new Yatzy(3, 2, 1, 1, 5));
-    }
-
-    @Test
-    public void onGameStartDiceIsOfLength5() {
-        Yatzy yatzy = new Yatzy(3, 4, 5, 6, 4);
-
-        assertEquals(5, yatzy.getDices());
-    }
-
-    @Test
-    public void onFirstDiceIsOne() {
-        assertEquals(1, yatzy.dices[0]);
-    }
-
-    @Test
-    public void onSecondDiceIsTwo() {
-        assertEquals(2, yatzy.dices[1]);
-    }
-
-    @Test
-    public void onThirdDiceIsThree() {
-        assertEquals(3, yatzy.dices[2]);
-    }
-
-    @Test
-    public void onFourthDiceIsFour() {
-        assertEquals(4, yatzy.dices[3]);
-    }
-
-    @Test
-    public void onLastDiceIsFive() {
-        assertEquals(5, yatzy.dices[4]);
-    }
-
-    @Test
-    public void onChance() {
-        assertEquals(0, yatzy.chance(0, 0, 0, 0, 0));
-    }
-
-    @Test
-    public void onFirstDiceChance() {
-        assertEquals(1, yatzy.chance(1, 0, 0, 0, 0));
-    }
-
-    @Test
-    public void onSecondDiceChance() {
-        assertEquals(3, yatzy.chance(1, 2, 0, 0, 0));
-    }
-
-    @Test
-    public void onThirdDiceChance() {
-        assertEquals(6, yatzy.chance(1, 2, 3, 0, 0));
-    }
-
-    @Test
-    public void onForthDiceChance() {
-        assertEquals(10, yatzy.chance(1, 2, 3, 4, 0));
-    }
-
-    @Test
-    public void onLastDiceChance() {
-        assertEquals(15, yatzy.chance(1, 2, 3, 4, 5));
-    }
-
-    @Test
-    public void onYatzyScores0() {
-        assertEquals(0, Yatzy.yatzy(6, 6, 6, 6, 1));
-    }
-
-    @Test
-    public void onYatzyScores50() {
-        assertEquals(50, Yatzy.yatzy(6, 6, 6, 6, 6));
-    }
-
-    @Test
-    public void onYatzyCountOnesIsZero() {
-        assertEquals(0, yatzy.ones(2, 2, 3, 4, 5));
-    }
-    @Test
-    public void onYatzyCountOnesFoundOne(){
-        assertEquals(1, yatzy.ones(1, 2, 3, 4, 5));
-    }
+  @Test
+  public void onNoArgs() {
+    assertNotNull(new Yatzy());
+  }
 
 
-    @Test
-    public void onYatzyCountOnesFoundTwo(){
-        assertEquals(2, yatzy.ones(1, 1, 3, 4, 5));
-    }
+  @Test
+  public void on5ArgsConstructor() {
+    assertNotNull(new Yatzy(3, 2, 1, 1, 5));
+  }
 
-    @Test
-    public void onYatzyCountOnesFoundThree(){
-        assertEquals(3, yatzy.ones(1, 1, 1, 4, 5));
-    }
+  @Test
+  public void onGameStartDiceIsOfLength5() {
+    Yatzy yatzy = new Yatzy(3, 4, 5, 6, 4);
+
+    assertEquals(5, yatzy.getDices());
+  }
+
+  @Test
+  public void onFirstDiceIsOne() {
+    assertEquals(1, yatzy.dices[0]);
+  }
+
+  @Test
+  public void onSecondDiceIsTwo() {
+    assertEquals(2, yatzy.dices[1]);
+  }
+
+  @Test
+  public void onThirdDiceIsThree() {
+    assertEquals(3, yatzy.dices[2]);
+  }
+
+  @Test
+  public void onFourthDiceIsFour() {
+    assertEquals(4, yatzy.dices[3]);
+  }
+
+  @Test
+  public void onLastDiceIsFive() {
+    assertEquals(5, yatzy.dices[4]);
+  }
+
+  @Test
+  public void onChance() {
+    assertEquals(0, yatzy.chance(0, 0, 0, 0, 0));
+  }
+
+  @Test
+  public void onFirstDiceChance() {
+    assertEquals(1, yatzy.chance(1, 0, 0, 0, 0));
+  }
+
+  @Test
+  public void onSecondDiceChance() {
+    assertEquals(3, yatzy.chance(1, 2, 0, 0, 0));
+  }
+
+  @Test
+  public void onThirdDiceChance() {
+    assertEquals(6, yatzy.chance(1, 2, 3, 0, 0));
+  }
+
+  @Test
+  public void onForthDiceChance() {
+    assertEquals(10, yatzy.chance(1, 2, 3, 4, 0));
+  }
+
+  @Test
+  public void onLastDiceChance() {
+    assertEquals(15, yatzy.chance(1, 2, 3, 4, 5));
+  }
+
+  @Test
+  public void onYatzyScores0() {
+    assertEquals(0, Yatzy.yatzy(6, 6, 6, 6, 1));
+  }
+
+  @Test
+  public void onYatzyScores50() {
+    assertEquals(50, Yatzy.yatzy(6, 6, 6, 6, 6));
+  }
+
+  @Test
+  public void onYatzyCountOnesIsZero() {
+    assertEquals(0, yatzy.ones(2, 2, 3, 4, 5));
+  }
+
+  @Test
+  public void onYatzyCountOnesFoundOne() {
+    assertEquals(1, yatzy.ones(1, 2, 3, 4, 5));
+  }
 
 
-    @Test
-    public void onYatzyCountOnesFoundFour(){
-        assertEquals(4, yatzy.ones(1, 1, 1, 1, 5));
-    }
+  @Test
+  public void onYatzyCountOnesFoundTwo() {
+    assertEquals(2, yatzy.ones(1, 1, 3, 4, 5));
+  }
 
-    @Test
-    public void onYatzyCountOnesFoundFive(){
-        assertEquals(5, yatzy.ones(1, 1, 1, 1, 1));
-    }
+  @Test
+  public void onYatzyCountOnesFoundThree() {
+    assertEquals(3, yatzy.ones(1, 1, 1, 4, 5));
+  }
+
+
+  @Test
+  public void onYatzyCountOnesFoundFour() {
+    assertEquals(4, yatzy.ones(1, 1, 1, 1, 5));
+  }
+
+  @Test
+  public void onYatzyCountOnesFoundFive() {
+    assertEquals(5, yatzy.ones(1, 1, 1, 1, 1));
+  }
+
+  @Test
+  public void onYatzyCountTwosFoundOne() {
+    assertEquals(2, yatzy.twos(2, 3, 3, 3, 3));
+  }
+
+  @Test
+  public void onYatzyCountTwosFoundTwo() {
+    assertEquals(4, yatzy.twos(2, 2, 3, 3, 3));
+  }
+
+  @Test
+  public void onYatzyCountTwosFoundThree() {
+    assertEquals(6, yatzy.twos(2, 2, 2, 3, 3));
+  }
+
+  @Test
+  public void onYatzyCountTwosFoundFour() {
+    assertEquals(8, yatzy.twos(2, 2, 2, 2, 3));
+  }
+
+  @Test
+  public void onYatzyCountTwosFoundFive() {
+    assertEquals(10, yatzy.twos(2, 2, 2, 2, 2));
+  }
+
+  @Test
+  public void onYatzyCountThreesFoundZero() {
+    assertEquals(0, yatzy.threes(1, 2, 2, 4, 5));
+  }
+
+  @Test
+  public void onYatzyCountThreesFoundOne() {
+    assertEquals(3, yatzy.threes(3, 2, 2, 4, 5));
+  }
+
+  @Test
+  public void onYatzyCountThreesFoundTwo() {
+    assertEquals(6, yatzy.threes(3, 3, 2, 4, 5));
+  }
+
+  @Test
+  public void onYatzyCountThreesFoundThree() {
+    assertEquals(9, yatzy.threes(3, 3, 3, 4, 5));
+  }
+
+  @Test
+  public void onYatzyCountThreesFoundFour() {
+    assertEquals(12, yatzy.threes(3, 3, 3, 3, 5));
+  }
+
+  @Test
+  public void onYatzyCountThreesFoundFive() {
+    assertEquals(15, yatzy.threes(3, 3, 3, 3, 3));
+  }
+
+  @Test
+  public void onNoFoursSumIsZero() {
+    yatzy = new Yatzy(1, 2, 3, 3, 5);
+    assertEquals(0, yatzy.fours());
+  }
 }
